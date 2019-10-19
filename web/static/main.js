5244c3481638c3485745f59d1c9e052bb4169312
You Gang Kuo <>
2019-10-19 20:33:23 +0800

    更新網路 icon 排列 功能 影片。

--- web/static/main.js
+++ web/static/main.js
@@ -1,1 +1,11 @@
 console.log("init main.js") ;
+
+var API_URL = "https://firms.modaps.eosdis.nasa.gov/" ;
+var API_KEY = "" ;
+var PROXY_URL = "https://scsonic.com:5000/" ;
+
+function recentWarning(callback) {
+    $.ajax(PROXY_URL, function() {
+        callback() ;
+    }) ;
+}
