f4c836cc7bb1c0baf51cf50848f6e6cbb21d05e9
You Gang Kuo <>
2019-10-23 18:18:13 +0800

    新增雜圖等功能 換地點 讀取 從firm來的csv檔。讀取分析結果。

--- web/static/main.js
+++ web/static/main.js
@@ -3,6 +3,7 @@ console.log("init main.js") ;
 var API_URL = "https://firms.modaps.eosdis.nasa.gov/" ;
 var API_KEY = "" ;
 var PROXY_URL = "https://scsonic.com:5000/" ;
+var firms_csv = "fire_archive_V1_82135.csv" ;
 
 function recentWarning(callback) {
     $.ajax(PROXY_URL, function() {
